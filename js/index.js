c64e3271e6b327026ebe5b100b4d6cb31e21bb9e
Maria25204 <lopez.galdamez2027@hilasal.superate.org>
2026-06-09 10:46:19 -0600

    origin

---
+++ js/index.js
@@ -0,0 +1,48 @@
+const cajita = document.getElementById("cajita")
+const boton = document.createElement("button")
+
+
+async function user() {
+const peticion = await fetch("https://randomuser.me/api/")   
+const respuesta = await peticion.json()
+
+const imagen = document.createElement("img")
+const nombre = document.createElement("h3")
+const correo = document.createElement("h4")
+const pais = document.createElement("p")
+
+
+imagen.src =  respuesta.results[0].picture.large
+nombre.textContent =  respuesta.results[0].name.first +  respuesta.results[0].name.last
+correo.textContent =  respuesta.results[0].email
+pais.textContent =  respuesta.results[0].location.country
+boton.textContent = "Random";
+
+const imagenVieja = document.querySelector("img")
+const nombreViejo = document.querySelector("h3")
+const correoViejo = document.querySelector("h4")
+const paisViejo = document.querySelector("p")
+
+cajita.appendChild(boton);
+
+cajita.appendChild(imagen);
+if(imagenVieja){
+    imagenVieja.remove()
+}
+cajita.appendChild(nombre);
+if(nombreViejo){
+    nombreViejo.remove()
+}
+cajita.appendChild(correo);
+if(correoViejo){
+    correoViejo.remove()
+}
+cajita.appendChild(pais); 
+if(paisViejo){
+    paisViejo.remove()
+}
+
+
+}
+user();
+boton.addEventListener("click", user)
